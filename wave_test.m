794e15946d429fc7f6dc4bcafdf7658ccebcd5af
Sidafa Conde <sconde@umassd.edu>
2014-06-09 12:37:50 -0400

    included BE, TRP, DWRK

--- wave_test.m
+++ wave_test.m
@@ -1,50 +1,40 @@
 close all;clear all;clc
 
 
-N = 64;
-cfl = 0.01;
-testmethod = '~/Dropbox/Research/ssp/sandbox/implicit/Methods/P2/S2/Downwindings2p2_r2.954950e+01.mat';
-dudx = SSP_Tools.Discretizers.WenoCore('kernel', 'WENO5', 'epsilon', 1e-16, 'p', 2);
+N = 128;
+cfl = 0.9;
+testmethod = '~/Dropbox/Research/ssp/sandbox/nonLinear/implicit/Methods/P2/S2/Downwindings2p2_r2.954950e+01.mat';
 dudt = SSP_Tools.Integrators.DWRK('coefficients', testmethod);
-%dudx = SSP_Tools.Discretizers.FiniteDifference();
-
+spacemethod = 'finiteDifference';
 %test = 'square_wave';
 test = 'sine_wave';
-switch test
-    case 'square_wave'
-        % spquare wave
-        problem = SSP_Tools.TestProblems.Advection('domain', [-1, 1], ...
-            'initial_condition', @(x)[ones(size(x(x<0))),zeros(size(x(x>=0)))],...
-            'discretizer', dudx, ...
-            'integrator', dudt, ...
-            'N', N , 'a',1);
-        
-    case 'sine_wave'
-        problem = SSP_Tools.TestProblems.Advection('domain', [-1, 1], ...
-            'initial_condition', @(x) sin(2*pi*x),...
-            'discretizer', dudx, ...
-            'integrator', dudt, ...
-            'N', N , 'a',1);
-        
-    otherwise
-        disp('not the right test')
-end
+
+spatialMethod; testProblem;
 
 t_end = 1.0;
+dx = min(diff(problem.x));
 dt = cfl* min(diff(problem.x));
+D = D/dx; Dt = Dt/dx;
 
 t_remaining = t_end - problem.t;
 
 tstep = 0;
 
 clf; hold on;
-axis ([-1 1 -1 1]);
-plot(problem.x,problem.u','-kx','LineWidth',2)
+axis ([0 1 -1 1]);
+plot(problem.x,problem.u','-.r','LineWidth',2)
 hold on
-plot(problem.x,problem.get_exact_solution,'-or','LineWidth',2)
+plot(problem.x,problem.get_exact_solution,'-k','LineWidth',2)
 title('Initial Start')
 
 legend('Approximation','Exact Solution')
+
+BE = speye(N+1)\(speye(N+1) - dt*D);
+TP = speye(N+1)\(speye(N+1) - 0.5*dt*D);
+TP = TP*(speye(N+1) + 0.5*dt*D);
+
+BEu0 = problem.u;
+TPu0 = problem.u;
 
 while t_remaining > 0
     if t_remaining > dt
@@ -54,19 +44,26 @@ while t_remaining > 0
     end
     problem.step(dt_step);
     tstep = tstep + 1;
+    BE_u = BE*BEu0;
+    TP_u = TP*TPu0;
     
     clf; hold on;
-    plot(problem.x,problem.u','-kx','LineWidth',2)
+    axis ([0 1 -1 1]);
+    plot(problem.x,problem.u','-.r','LineWidth',2)
     hold on
-    plot(problem.x,problem.get_exact_solution,'-or','LineWidth',2)
+    plot(problem.x,problem.get_exact_solution,'-k','LineWidth',2)
+    plot(problem.x,BE_u,'--b','LineWidth',2)
+    plot(problem.x,TP_u,'-xg','LineWidth',2)
     title(sprintf('Time Step %d',tstep))
-    legend('Approximation','Exact Solution')
-
+    legend('Approximation','Exact Solution','BE','2nd order RK')
+    
     drawnow;
     print(gcf,'-dpng',sprintf('results/%s%04d',test,tstep))
     
     
     t_remaining = t_end - problem.t;
     hold off
+    BEu0 = BE_u;
+    TPu0 = TP_u;
 end
 
